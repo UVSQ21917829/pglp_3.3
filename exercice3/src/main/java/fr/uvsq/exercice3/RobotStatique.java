770f2b5b9eacbc1f245b3e516abde82afacabca5
ait ihya <hajaraitihya@gmail.com>
2020-02-26 11:49:03 +0100

    class robotstatique

---
+++ exercice3/src/main/java/fr/uvsq/exercice3/RobotStatique.java
@@ -0,0 +1,14 @@
+package fr.uvsq.exercice3;
+
+public class RobotStatique {
+	private int position;
+
+	public RobotStatique(int position) {
+		this.position = position;
+	}
+	 public void avance() {
+		 throw new UnsupportedOperationException();
+	 }
+	
+
+}
